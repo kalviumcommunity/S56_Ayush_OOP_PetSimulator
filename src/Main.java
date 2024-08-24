c1d45235dc86b3bfee02b1469b874ba0bebaa3f0
Ayush9535 <ghodkeayush22@gmail.com>
2024-08-24 16:57:27 +0530

    New keyword used to allocate memory dynamically

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,26 @@
 public class Main {
     public static void main(String[] args) {
 
+        // Dynamic Allocation of Memory for Array of Objects
         Pet[] pets = new Pet[3];
 
-        // Creating 1st pet : Ben the Dog
+        // Dynamic Allocation of Memory using the new keyword
         Dog Tuffy = new Dog("Tuffy");
         pets[0] = Tuffy;
-
-        // Creating 2nd pet : Tom the Cat
+        
+        // Dynamic Allocation of Memory using the new keyword
         Cat Tom = new Cat("Tom");
         pets[1] = Tom;
-
-        // Creating 3rd pet : Hank the Dog
+        
+        // Dynamic Allocation of Memory using the new keyword
         Dog Sheru = new Dog("Sheru");
         pets[2] = Sheru;
 
         for(Pet pet : pets){
             pet.makeSound();
         }
+
+        // Deallocation is done automatically in Java by the process of Garbage Collection. 
         
     }
 }
