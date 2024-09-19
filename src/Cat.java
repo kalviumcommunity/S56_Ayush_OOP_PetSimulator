fa06bcbe7f3b71bc8f33cdb124569f2cf4cb2800
Ayush9535 <ghodkeayush22@gmail.com>
2024-09-19 14:14:48 +0530

    Encapsulation

--- src/Cat.java
+++ src/Cat.java
@@ -4,10 +4,6 @@ public class Cat extends Pet{
         super(name);
     }
 
-    public String getName(){
-        return this.name;
-    }
-
     @Override   
     public void makeSound(){
         System.out.println(this.getName() + " says: Hello!, I am Cat");
